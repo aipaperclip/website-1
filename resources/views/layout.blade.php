79124e3951cd5479eab3288e2b7af820063313e4
unknown <miroslav.nedelchev@dentacoin.com>
2018-08-31 16:44:35 +0300

    uploading to live 1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -123,13 +123,13 @@
             </div>
         </div>
     </footer>
-    <script src="/dist/js/front-libs-script.js"></script>
     <!-- Zendesk Widget script -->
     <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="dentacoin.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
         /*]]>*/</script>
     <!-- End of Zendesk Widget script -->
     {{--<script src="/assets/js/basic.js"></script>--}}
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd5xOHXvqHKf8ulbL8hEhFA4kb7H6u6D4" type="text/javascript"></script>
+    <script src="/dist/js/front-libs-script.js"></script>
     @yield("script_block")
     <script src="/dist/js/front-script.js"></script>
 {{--<script src="/assets/js/markerclusterer-v2.js"></script>
