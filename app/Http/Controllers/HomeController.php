8b45a0deef09080e10ea3b3eb5ea00958f9b7b20
unknown <miroslav.nedelchev@dentacoin.com>
2018-09-17 17:24:39 +0300

    fixed intro dentists dynamic number

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,11 +15,12 @@ class HomeController extends Controller
     protected function getView()   {
         $latest_blog_articles = DB::connection('mysql2')->select(DB::raw("SELECT `post_title`, `post_name` from dIf_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY `post_date` DESC LIMIT 0, 5"));
         $params = ['applications' => $this->getApplications(), 'testimonials' => $this->getFeaturedTestimonials(), 'publications' => $this->getPublications(), 'latest_blog_articles' => $latest_blog_articles, 'exchange_platforms' => (new AvailableBuyingOptionsController())->getExchangePlatforms(), 'wallets' => (new AvailableBuyingOptionsController())->getWallets()];
-        //$url = 'https://reviews.dentacoin.com/';
-        //$html = file_get_contents($url);
-        //preg_match_all('/[^>]class=["\']second[\'"]*>(.*?)<\//', $html, $matches,PREG_PATTERN_ORDER);
-        //$params['dental_practices'] = $matches[1][0];
-//
+        $url = 'https://reviews.dentacoin.com/';
+        $html = file_get_contents($url);
+        $sep1 = explode('<b class="second">', $html);
+        $set2 = explode("</b>", $sep1[1]);
+        var_dump($set2[0]);die();
+
         return view("pages/homepage", $params);
     }
 
