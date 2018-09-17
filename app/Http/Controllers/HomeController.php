c37ba103a5ee6115daadf5ae57d235bed2b0a3de
unknown <miroslav.nedelchev@dentacoin.com>
2018-09-17 11:58:51 +0300

    fixing problem in live

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,18 +17,16 @@ class HomeController extends Controller
         $page = @file_get_contents($url);
         $latest_blog_articles = DB::connection('mysql2')->select(DB::raw("SELECT `post_title`, `post_name` from dIf_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY `post_date` DESC LIMIT 0, 5"));
         $params = ['applications' => $this->getApplications(), 'testimonials' => $this->getFeaturedTestimonials(), 'publications' => $this->getPublications(), 'latest_blog_articles' => $latest_blog_articles, 'exchange_platforms' => (new AvailableBuyingOptionsController())->getExchangePlatforms(), 'wallets' => (new AvailableBuyingOptionsController())->getWallets()];
-        if($page) {
-            $dom = new \DOMDocument();
-            $internalErrors = libxml_use_internal_errors(true);
-            $dom->loadhtml($page);
-            libxml_use_internal_errors($internalErrors);
-            $xpath = new \DomXPath($dom);
-            $nodeList = $xpath->query("//b[@class='second']");
-            $node = $nodeList->item(0);
-            var_dump($node);
-            die();
-            $params['dental_practices'] = $node->nodeValue;
-        }
+        //if($page) {
+        //    $dom = new \DOMDocument();
+        //    $internalErrors = libxml_use_internal_errors(true);
+        //    $dom->loadhtml($page);
+        //    libxml_use_internal_errors($internalErrors);
+        //    $xpath = new \DomXPath($dom);
+        //    $nodeList = $xpath->query("//b[@class='second']");
+        //    $node = $nodeList->item(0);
+        //    $params['dental_practices'] = $node->nodeValue;
+        //}
         return view("pages/homepage", $params);
     }
 
