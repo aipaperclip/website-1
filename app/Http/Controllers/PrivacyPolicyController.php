84b8785dbb2fc6a4ff04fc0c1daca845cf38e51d
unknown <miroslav.nedelchev@dentacoin.com>
2018-09-18 15:36:14 +0300

    test server IP

--- app/Http/Controllers/PrivacyPolicyController.php
+++ app/Http/Controllers/PrivacyPolicyController.php
@@ -9,6 +9,17 @@ use Illuminate\Http\Request;
 class PrivacyPolicyController extends Controller
 {
     protected function getView()   {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        var_dump($ip);
+        var_dump($_SERVER['SERVER_ADDR']);
+        var_dump($_SERVER['SERVER_PORT']);
+        die();
         return view('pages/privacy-policy');
     }
 }
