3f48c2f60f572c8975cfa32e2cdc61584bb3f498
unknown <miroslav.nedelchev@dentacoin.com>
2018-10-04 17:28:46 +0300

    footer edit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -895,7 +895,7 @@ function stopMaliciousInspect()  {
         }
     }
 }
-stopMaliciousInspect();
+//stopMaliciousInspect();
 
 function hidePopupOnBackdropClick() {
     $(document).on('click', '.bootbox', function(){
