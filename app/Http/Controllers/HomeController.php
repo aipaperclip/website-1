7bb09abf2a97ff8ec16d318bdad0462054bb174f
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-10 11:00:58 +0200

    test bug missing blog articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,8 @@ class HomeController extends Controller
 {
     protected function getView()   {
         $latest_blog_articles = DB::connection('mysql2')->select(DB::raw("SELECT `post_title`, `post_name` from dIf_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY `post_date` DESC LIMIT 0, 5"));
+        var_dump($latest_blog_articles);
+        die();
         $params = ['applications' => $this->getApplications(), 'testimonials' => $this->getFeaturedTestimonials(), 'publications' => $this->getPublications(), 'latest_blog_articles' => $latest_blog_articles, 'exchange_platforms' => (new AvailableBuyingOptionsController())->getExchangePlatforms(), 'wallets' => (new AvailableBuyingOptionsController())->getWallets()];
         return view("pages/homepage", $params);
     }
