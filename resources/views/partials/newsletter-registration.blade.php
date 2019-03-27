219169b43f2775233a381c42a198cb1c698a0516
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-27 15:48:23 +0200

    added checkbox to newsletter

--- resources/views/partials/newsletter-registration.blade.php
+++ resources/views/partials/newsletter-registration.blade.php
@@ -14,7 +14,7 @@
     </div>
 </div>--}}
 <div class="row newsletter-register">
-    <div class="col-xs-12 text-center padding-top-50">
+    <div class="col-xs-12 text-center">
         <div id="mc_embed_signup">
             <form action="https://dentacoin.us16.list-manage.com/subscribe/post?u=61ace7d2b009198ca373cb213&amp;id=993df5967d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
                 <div id="mc_embed_signup_scroll">
