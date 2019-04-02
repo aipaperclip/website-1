8f94aef5cf3c0e67554d931f60c635ae3ed8bbba
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-02 22:31:14 +0300

    changed civic api url

--- public/assets/libs/civic-login/civic-config.js
+++ public/assets/libs/civic-login/civic-config.js
@@ -1,5 +1,5 @@
 const civic_config = {
     app_id: 'rkvErCDdf',
-    url_exchange_token_for_data: 'https://dentacoin.net/civic',
+    url_exchange_token_for_data: 'https://civic.dentacoin.net/civic',
     platform: 'civic'
 };
