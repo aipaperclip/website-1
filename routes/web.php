f39af8e668b664c7c3dedc9eac9de08beac2f47e
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-24 09:03:24 +0300

    test session

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::group(['prefix' => '/', 'middleware' => 'frontEndMiddleware'], function (
 
     Route::get('/', 'HomeController@getView')->name('home');
 
-    Route::get('/home1', 'HomeController@getView1')->name('home1');
-
     Route::get('/hub', 'HomeController@getLoggedView')->middleware('HandleUserSession')->name('hub');
 
     Route::get('/foundation', 'HomeController@getView')->middleware('HandleUserSession')->name('foundation');
