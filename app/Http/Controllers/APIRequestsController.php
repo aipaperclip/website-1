aded14dab59a0733e35e10e07bfd3e243eb76d92
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-08 11:19:41 +0300

    test dentacoin withdraw

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -364,12 +364,8 @@ class APIRequestsController extends Controller {
             CURLOPT_HTTPHEADER => $header
         ));
 
-        //$resp = json_decode(curl_exec($curl));
-        $resp = curl_exec($curl);
+        $resp = json_decode(curl_exec($curl));
         curl_close($curl);
-
-        var_dump($resp);
-        die();
 
         if(!empty($resp))   {
             return $resp;
@@ -396,8 +392,12 @@ class APIRequestsController extends Controller {
             CURLOPT_HTTPHEADER => $header
         ));
 
-        $resp = json_decode(curl_exec($curl));
+        //$resp = json_decode(curl_exec($curl));
+        $resp = curl_exec($curl);
         curl_close($curl);
+
+        var_dump($resp);
+        die();
 
         if(!empty($resp))   {
             return $resp;
