18dad140cbc934612610f9b52087dd9435dd102a
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-09 08:13:14 +0300

    fix 404 page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -31,7 +31,7 @@ class Controller extends BaseController
     const currencies = ['USD', 'EUR', 'GBP', 'RUB', 'INR', 'CNY', 'JPY'];
 
     public function __construct() {
-        if(!Request::isMethod('post'))    {
+        if(!empty(Route::getCurrentRoute()) && !Request::isMethod('post'))    {
             View::share('mobile', $this->isMobile());
             View::share('meta_data', $this->getMetaData());
             View::share('parent_titles', $this->getParentDbTitles());
