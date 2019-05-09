a38a21fa5c9ee8303580d1356d9286c9d5e9763a
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-09 14:50:40 +0300

    test custom cookie

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -592,8 +592,8 @@ class UserController extends Controller {
     //dentist can add profile description while waiting for approval from Dentacoin admin
     protected function setCustomCookie(Request $request) {
         if(!empty(Input::get('slug'))) {
-            var_dump(Input::get('slug'));
-            var_dump($this->decrypt(Input::get('slug')));
+            $user = $this->decrypt(urldecode(Input::get('slug')));
+            var_dump($user);
             die();
         } else {
             return abort(404);
