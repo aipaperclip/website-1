00fa15786d0e619baa5c860c1c25c239a2268f8b
unknown <miroslav.nedelchev@dentacoin.com>
2019-11-08 17:30:11 +0200

    fix civic login method

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -183,10 +183,10 @@
                     <div class="form-login">
                         <h2>LOG IN</h2>
                         <div class="padding-bottom-10">
-                            <a href="javascript:void(0)" class="facebook-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/register" data-platform="dentacoin" @if(isset($inviter)) data-inviter="{{$inviter}}" @endif data-type="patient">with Facebook</a>
+                            <a href="javascript:void(0)" class="facebook-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/login" data-platform="dentacoin" @if(isset($inviter)) data-inviter="{{$inviter}}" @endif data-type="patient">with Facebook</a>
                         </div>
                         <div>
-                            <a href="javascript:void(0)"  class="civic-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/register" data-platform="dentacoin" @if(isset($inviter)) data-inviter="{{$inviter}}" @endif data-type="patient">with Civic</a>
+                            <a href="javascript:void(0)"  class="civic-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/login" data-platform="dentacoin" @if(isset($inviter)) data-inviter="{{$inviter}}" @endif data-type="patient">with Civic</a>
                         </div>
                         <div class="popup-half-footer">
                             Don't have an account? <a href="javascript:void(0)" class="call-sign-up color-white">Sign up</a>
