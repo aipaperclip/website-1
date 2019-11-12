bb4ac530b451b537c167cc47dfcbee267c614f16
unknown <miroslav.nedelchev@dentacoin.com>
2019-11-12 11:43:49 +0200

    test calendar

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1115,31 +1115,33 @@ if(($('body').hasClass('home') && !$('body').hasClass('logged-in')) || ($('body'
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
-    FB.login(function(response) {
-        console.log(2);
-        if (response.session) {
-            console.log(response.session, 'response.session');
-            var user_id = response.session.uid;
-            var page_id = "40796308305"; //coca cola
-            var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
-            var the_query = FB.Data.query(fql_query);
+    $(document).ready(function() {
+        FB.login(function(response) {
+            console.log(2);
+            if (response.session) {
+                console.log(response.session, 'response.session');
+                var user_id = response.session.uid;
+                var page_id = "40796308305"; //coca cola
+                var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
+                var the_query = FB.Data.query(fql_query);
 
-            the_query.wait(function(rows) {
+                the_query.wait(function(rows) {
 
-                if (rows.length == 1 && rows[0].uid == user_id) {
-                    $("#container_like").show();
+                    if (rows.length == 1 && rows[0].uid == user_id) {
+                        $("#container_like").show();
 
-                    //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
+                        //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
 
-                } else {
-                    $("#container_notlike").show();
-                    //and here you could get the content for a non liker in ajax...
-                }
-            });
-        } else {
-            // user is not logged in
-        }
-    });
+                    } else {
+                        $("#container_notlike").show();
+                        //and here you could get the content for a non liker in ajax...
+                    }
+                });
+            } else {
+                // user is not logged in
+            }
+        });
+    })
 }
 
 function drawHeaderToFirstSectionLine() {
