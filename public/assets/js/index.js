2b5923b410f3b63b039ebe52d7319abcff71a557
unknown <miroslav.nedelchev@dentacoin.com>
2019-11-12 11:41:20 +0200

    test calendar

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1094,38 +1094,51 @@ if(($('body').hasClass('home') && !$('body').hasClass('logged-in')) || ($('body'
     }
 } else if($('body.christmas-calendar').length > 0) {
     // CHRISTMAS CALENDAR
-
-    $(document).ready(function(){
-        console.log(1);
-        FB.login(function(response) {
-
-            console.log(2);
-            if (response.session) {
-                console.log(response.session, 'response.session');
-                var user_id = response.session.uid;
-                var page_id = "40796308305"; //coca cola
-                var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
-                var the_query = FB.Data.query(fql_query);
-
-                the_query.wait(function(rows) {
-
-                    if (rows.length == 1 && rows[0].uid == user_id) {
-                        $("#container_like").show();
-
-                        //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
-
-                    } else {
-                        $("#container_notlike").show();
-                        //and here you could get the content for a non liker in ajax...
-                    }
-                });
-
-
-            } else {
-                // user is not logged in
-            }
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '1906201509652855',
+            cookie: true,
+            xfbml: true,
+            version: 'v2.8'
         });
+        FB.AppEvents.logPageView();
+    };
 
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = '//connect.facebook.net/bg_BG/sdk.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    FB.login(function(response) {
+        console.log(2);
+        if (response.session) {
+            console.log(response.session, 'response.session');
+            var user_id = response.session.uid;
+            var page_id = "40796308305"; //coca cola
+            var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
+            var the_query = FB.Data.query(fql_query);
+
+            the_query.wait(function(rows) {
+
+                if (rows.length == 1 && rows[0].uid == user_id) {
+                    $("#container_like").show();
+
+                    //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
+
+                } else {
+                    $("#container_notlike").show();
+                    //and here you could get the content for a non liker in ajax...
+                }
+            });
+        } else {
+            // user is not logged in
+        }
     });
 }
 
