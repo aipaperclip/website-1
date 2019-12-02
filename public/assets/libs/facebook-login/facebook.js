d406e444c42e1ce16008002799b4737b7edfed30
unknown <miroslav.nedelchev@dentacoin.com>
2019-12-02 18:23:02 +0200

    test fb login

--- public/assets/libs/facebook-login/facebook.js
+++ public/assets/libs/facebook-login/facebook.js
@@ -101,6 +101,8 @@ async function initFacebookLogin() {
     });
 }
 
+initFacebookLogin();
+
 /*(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {
