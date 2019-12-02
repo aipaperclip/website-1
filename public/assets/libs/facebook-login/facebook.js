982323c57a61e11fc2ecdeb872b3b8928ba5479c
unknown <miroslav.nedelchev@dentacoin.com>
2019-12-02 18:28:32 +0200

    test fb login

--- public/assets/libs/facebook-login/facebook.js
+++ public/assets/libs/facebook-login/facebook.js
@@ -4,105 +4,23 @@ const fb_config = {
     platform: 'fb'
 };
 
-/*if ($('#facebook-jssdk').length) {
+if ($('#facebook-jssdk').length) {
     $('#facebook-jssdk').remove();
 }
 
-$('head').append('<script id="facebook-jssdk" src=""></script>');*/
+$('head').append('<script id="facebook-jssdk" src="//connect.facebook.net/bg_BG/sdk.js"></script>');
 
-async function initFacebookLogin() {
-    console.log('initFacebookLogin');
-    await $.getScript('//connect.facebook.net/bg_BG/sdk.js', function() {
-        console.log('getScript');
-        //application init
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: fb_config.app_id,
-                cookie: true,
-                xfbml: true,
-                version: 'v2.8'
-            });
-
-            console.log('window.fbAsyncInit');
-
-            //binding click event for all the faceboon login btns
-            $('body').on('click', '.facebook-custom-btn', function(rerequest){
-                var this_btn = $(this);
-                customFacebookEvent('facebookCustomBtnClicked', 'Button #facebook-custom-btn was clicked.');
-
-                //based on some logic and conditions you can add or remove this attribute, if custom-stopped="true" the facebook login won't proceed
-                if($(this).attr('custom-stopper') && $(this).attr('custom-stopper') == 'true') {
-                    customFacebookEvent('customCivicFbStopperTriggered', '');
-                    return false;
-                }
-
-                FB.login(function (response) {
-                    if(response.authResponse && response.status == 'connected') {
-                        customFacebookEvent('receivedFacebookToken', 'Received facebook token successfully.', response);
-
-                        var fb_token = response.authResponse.accessToken;
-
-                        var register_data = {
-                            platform: this_btn.attr('data-platform'),
-                            social_network: fb_config.platform,
-                            auth_token: fb_token,
-                            type: this_btn.attr('data-type')
-                        };
-
-                        if(this_btn.attr('data-inviter') != undefined) {
-                            register_data.invited_by = this_btn.attr('data-inviter');
-                        }
-
-                        //exchanging the token for user data
-                        $.ajax({
-                            type: 'POST',
-                            dataType: 'json',
-                            url: this_btn.attr('data-url'),
-                            data: register_data,
-                            success: function(data){
-                                if (data.success) {
-                                    //firing success event
-                                    customFacebookEvent('successResponseCoreDBApi', 'Request to CoreDB-API succeed.', data);
-                                } else {
-                                    //firing error event
-                                    customFacebookEvent('errorResponseCoreDBApi', 'Request to CoreDB-API succeed, but conditions failed.', data);
-                                }
-                            },
-                            error: function() {
-                                //ajax to the external url is not working properly
-                                customFacebookEvent('noCoreDBApiConnection', 'Request to CoreDB-API failed.');
-                            }
-                        });
-                    }
-                });
-            });
-
-//exchanging token for data
-            /*function fbGetData() {
-                FB.api('/me?fields=id,email,name,permissions', function (response) {
-                    customFacebookEvent('receivedFacebookData', 'Received facebook data successfully.', response);
-                });
-            }*/
-
-//custom function for firing events
-            function customFacebookEvent(type, message, response_data) {
-                var event_obj = {
-                    type: type,
-                    message: message,
-                    platform_type: 'facebook',
-                    time: new Date()
-                };
-
-                if(response_data != undefined) {
-                    event_obj.response_data = response_data;
-                }
-                $.event.trigger(event_obj);
-            }
-        };
+//application init
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: fb_config.app_id,
+        cookie: true,
+        xfbml: true,
+        version: 'v2.8'
     });
-}
 
-initFacebookLogin();
+    console.log('window.fbAsyncInit');
+};
 
 /*(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -114,4 +32,78 @@ initFacebookLogin();
     js.src = '//connect.facebook.net/bg_BG/sdk.js';
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));*/
+
+//binding click event for all the faceboon login btns
+$('body').on('click', '.facebook-custom-btn', function(rerequest){
+    var this_btn = $(this);
+    customFacebookEvent('facebookCustomBtnClicked', 'Button #facebook-custom-btn was clicked.');
+
+    //based on some logic and conditions you can add or remove this attribute, if custom-stopped="true" the facebook login won't proceed
+    if($(this).attr('custom-stopper') && $(this).attr('custom-stopper') == 'true') {
+        customFacebookEvent('customCivicFbStopperTriggered', '');
+        return false;
+    }
+
+    FB.login(function (response) {
+        if(response.authResponse && response.status == 'connected') {
+            customFacebookEvent('receivedFacebookToken', 'Received facebook token successfully.', response);
+
+            var fb_token = response.authResponse.accessToken;
+
+            var register_data = {
+                platform: this_btn.attr('data-platform'),
+                social_network: fb_config.platform,
+                auth_token: fb_token,
+                type: this_btn.attr('data-type')
+            };
+
+            if(this_btn.attr('data-inviter') != undefined) {
+                register_data.invited_by = this_btn.attr('data-inviter');
+            }
+
+            //exchanging the token for user data
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: this_btn.attr('data-url'),
+                data: register_data,
+                success: function(data){
+                    if (data.success) {
+                        //firing success event
+                        customFacebookEvent('successResponseCoreDBApi', 'Request to CoreDB-API succeed.', data);
+                    } else {
+                        //firing error event
+                        customFacebookEvent('errorResponseCoreDBApi', 'Request to CoreDB-API succeed, but conditions failed.', data);
+                    }
+                },
+                error: function() {
+                    //ajax to the external url is not working properly
+                    customFacebookEvent('noCoreDBApiConnection', 'Request to CoreDB-API failed.');
+                }
+            });
+        }
+    });
+});
+
+//exchanging token for data
+/*function fbGetData() {
+    FB.api('/me?fields=id,email,name,permissions', function (response) {
+        customFacebookEvent('receivedFacebookData', 'Received facebook data successfully.', response);
+    });
+}*/
+
+//custom function for firing events
+function customFacebookEvent(type, message, response_data) {
+    var event_obj = {
+        type: type,
+        message: message,
+        platform_type: 'facebook',
+        time: new Date()
+    };
+
+    if(response_data != undefined) {
+        event_obj.response_data = response_data;
+    }
+    $.event.trigger(event_obj);
+}
 
