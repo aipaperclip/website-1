1891b4f62301f25f31c9743050dde92604fe36cd
unknown <miroslav.nedelchev@dentacoin.com>
2019-12-04 14:20:34 +0200

    test os

--- resources/views/pages/test-os.blade.php
+++ resources/views/pages/test-os.blade.php
@@ -2,19 +2,19 @@
 @section("content")
     <div class="padding-top-100 padding-bottom-200 padding-left-20 padding-right-20 container">
         <div class="padding-bottom-50">
-            <button type="button" ca0ss="dark-blue-white-btn test-mobile">Test isMobile</button>
+            <button type="button" class="white-dark-blue-btn test-mobile">Test isMobile</button>
         </div>
         <div class="padding-bottom-50">
-            <button type="button" ca0ss="dark-blue-white-btn test-os-btn">Test OS</button>
+            <button type="button" class="white-dark-blue-btn test-os-btn">Test OS</button>
         </div>
         <div class="padding-bottom-50">
-            <button type="button" ca0ss="dark-blue-white-btn test-userAgent">Test navigator.userAgent </button>
+            <button type="button" class="white-dark-blue-btn test-userAgent">Test navigator.userAgent </button>
         </div>
         <div class="padding-bottom-50">
-            <button type="button" ca0ss="dark-blue-white-btn test-vendor">Test navigator.vendor </button>
+            <button type="button" class="white-dark-blue-btn test-vendor">Test navigator.vendor </button>
         </div>
         <div class="padding-bottom-50">
-            <button type="button" class="dark-blue-white-btn test-opera">Test window opera </button>
+            <button type="button" class="white-dark-blue-btn test-opera">Test window opera </button>
         </div>
     </div>
 @endsection
