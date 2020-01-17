e7afa85b5335950116c9e007d195b7c1dbb06969
Mediabasket <office@mediabasket.eu>
2020-01-17 11:06:16 +0200

    add JobPosting meta schema for /careers page

--- resources/views/pages/careers.blade.php
+++ resources/views/pages/careers.blade.php
@@ -10,14 +10,16 @@
                 <div class="col-xs-12 col-sm-6 inline-block image">
                     <div class="second-dot fs-16 inline-block">&nbsp;</div>
                     <figure itemscope="" itemtype="http://schema.org/ImageObject">
-                        <img src="{{URL::asset('assets/images/person-in-magnifier.svg') }}" alt="Person in magnifier" itemprop="contentUrl"/>
+                        <img src="{{URL::asset('assets/images/person-in-magnifier.svg') }}" alt="Person in magnifier"
+                             itemprop="contentUrl"/>
                         <div class="third-dot fs-16 inline-block">&nbsp;</div>
                     </figure>
                 </div>
                 <div class="col-xs-12 col-sm-6 inline-block text">
                     <div class="section-subtitle">{{$titles[0]->html}}</div>
                     <div class="section-description">{!! $sections[0]->html !!}</div>
-                    <div class="btn-container"><a href="javascript:void(0)" class="white-blue-rounded-btn">{{$sections[1]->html}}</a></div>
+                    <div class="btn-container"><a href="javascript:void(0)"
+                                                  class="white-blue-rounded-btn">{{$sections[1]->html}}</a></div>
                 </div>
             </div>
             @include('partials.benefits', ['borders' => true])
@@ -25,7 +27,8 @@
                 <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
                     <div class="logo-over-line">
                         <figure itemscope="" itemtype="http://schema.org/ImageObject">
-                            <img src="{{URL::asset('assets/images/logo.svg') }}" alt="Dentacoin section logo" itemprop="contentUrl"/>
+                            <img src="{{URL::asset('assets/images/logo.svg') }}" alt="Dentacoin section logo"
+                                 itemprop="contentUrl"/>
                         </figure>
                         <div class="border-behind-logo"></div>
                     </div>
@@ -34,28 +37,43 @@
             </div>
             <div class="row open-job-positions">
                 <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-                @if(sizeof($job_offers) > 0)
-                    <div class="row fs-0">
-                        @foreach($job_offers as $job_offer)
-                            <div class="col-xs-12 col-sm-4 single inline-block-top">
-                                <div class="wrapper">
-                                    <a href="{{route('careers', ['slug' => $job_offer->slug])}}">
-                                        <figure itemscope="" itemtype="http://schema.org/ImageObject">
-                                            <img src="{{URL::asset('assets/uploads/'.$job_offer->media->name) }}" alt="{{$job_offer->media->alt}}" itemprop="contentUrl"/>
-                                            <figcaption>{{$job_offer->title}}</figcaption>
-                                        </figure>
-                                    </a>
-                                    <div class="border"></div>
-                                    <div class="btn-container">
-                                        <a href="{{route('careers', ['slug' => $job_offer->slug])}}" class="white-blue-rounded-btn">APPLY</a>
+                    @if(sizeof($job_offers) > 0)
+                        <div class="row fs-0">
+                            @foreach($job_offers as $job_offer)
+                                <div class="col-xs-12 col-sm-4 single inline-block-top" itemscope=""
+                                     itemtype="http://schema.org/JobPosting">
+                                    @if(!empty($job_offer->remote_work))
+                                        <meta itemprop="employmentType" content="{{$job_offer->remote_work}}">
+                                    @endif
+                                    <meta itemprop="title" content="{{$job_offer->title}}">
+                                    <meta itemprop="datePosted" content="{{$job_offer->created_at}}">
+                                    @if(!empty($job_offer->location))
+                                        <div itemprop="jobLocation" itemscope itemtype="http://schema.org/Place">
+                                            <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                                                <meta itemprop="addressLocality" content="{{$job_offer->location}}">
+                                            </div>
+                                        </div>
+                                    @endif
+                                    <div class="wrapper">
+                                        <a href="{{route('careers', ['slug' => $job_offer->slug])}}">
+                                            <figure itemscope="" itemtype="http://schema.org/ImageObject">
+                                                <img src="{{URL::asset('assets/uploads/'.$job_offer->media->name) }}"
+                                                     alt="{{$job_offer->media->alt}}" itemprop="contentUrl"/>
+                                                <figcaption>{{$job_offer->title}}</figcaption>
+                                            </figure>
+                                        </a>
+                                        <div class="border"></div>
+                                        <div class="btn-container">
+                                            <a href="{{route('careers', ['slug' => $job_offer->slug])}}"
+                                               class="white-blue-rounded-btn">APPLY</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
-                    </div>
-                @else
-                    <div class="col-xs-12 no-results">{!! $sections[2]->html !!}</div>
-                @endif
+                            @endforeach
+                        </div>
+                    @else
+                        <div class="col-xs-12 no-results">{!! $sections[2]->html !!}</div>
+                    @endif
                 </div>
             </div>
         </div>
