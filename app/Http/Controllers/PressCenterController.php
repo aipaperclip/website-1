af5026d6d35c7a70dce7e8131dc2dd536e113d6c
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-06 15:58:51 +0200

    test careers submit form

--- app/Http/Controllers/PressCenterController.php
+++ app/Http/Controllers/PressCenterController.php
@@ -77,24 +77,26 @@ class PressCenterController extends Controller
             return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. Please try again with valid email.']);
         }
         if(!empty($files))    {
-            //404 if they're trying to send more than 2 files
-            if(sizeof($files) > 2) {
-                return abort(404);
-            }else {
-                $allowed = array('pdf', 'doc', 'docx', 'ppt', 'pptx', 'odt', 'rtf', 'xls', 'xlsx', 'PDF', 'DOC', 'DOCX', 'PPT', 'PPTX', 'ODT', 'RTF', 'XLS', 'XLSX');
-                foreach($files as $file)  {
-                    //checking the file size
-                    if($file->getSize() > MAX_UPL_SIZE) {
-                        return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. Files can be only with with maximum size of '.number_format(MAX_UPL_SIZE / 1048576).'MB. Please try again.']);
-                    }
-                    //checking file format
-                    if(!in_array(pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION), $allowed)) {
-                        return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. Files can be only with .pdf, .doc, docx, ppt, pptx, odt, rtf, xls or xlsx formats. Please try again.']);
-                    }
-                    //checking if error in file
-                    if($file->getError()) {
-                        return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. There is error with one or more of the files, please try with other files. Please try again.']);
-                    }
+            $fileCounter = 0;
+            $allowed = array('pdf', 'doc', 'docx', 'ppt', 'pptx', 'odt', 'rtf', 'xls', 'xlsx', 'PDF', 'DOC', 'DOCX', 'PPT', 'PPTX', 'ODT', 'RTF', 'XLS', 'XLSX');
+            foreach($files as $file)  {
+                // doing this check to prevent people submitting move than one file
+                $fileCounter+=1;
+                if($fileCounter > 2) {
+                    return abort(404);
+                }
+
+                //checking the file size
+                if($file->getSize() > MAX_UPL_SIZE) {
+                    return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. Files can be only with with maximum size of '.number_format(MAX_UPL_SIZE / 1048576).'MB. Please try again.']);
+                }
+                //checking file format
+                if(!in_array(pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION), $allowed)) {
+                    return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. Files can be only with .pdf, .doc, docx, ppt, pptx, odt, rtf, xls or xlsx formats. Please try again.']);
+                }
+                //checking if error in file
+                if($file->getError()) {
+                    return redirect()->route('press-center', ['id' => 1])->with(['error' => 'Your form was not sent. There is error with one or more of the files, please try with other files. Please try again.']);
                 }
             }
         }
@@ -107,7 +109,7 @@ class PressCenterController extends Controller
             $message->from($data['email'], $data['sender-name'])->replyTo($data['email'], $data['sender-name']);
             $message->setBody($body, 'text/html');
 
-            if(!empty($files) && sizeof($files > 0)) {
+            if(!empty($files)) {
                 foreach($files as $file) {
                     $message->attach($file->getRealPath(), array(
                         'as' => $file->getClientOriginalName(), // If you want you can change original name to custom name
