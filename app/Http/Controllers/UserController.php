4d34b83d79d09b8ee7faf8617ad4777607afbb80
unknown <miroslav.nedelchev@dentacoin.com>
2020-04-07 18:18:07 +0300

    update maintanance mode page and test combined login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -440,6 +440,7 @@ class UserController extends Controller {
     protected function getCountryCode() {
         $ip = $this->getClientIp();
         var_dump($ip);
+        var_dump('https://ipinfo.io/' . $ip . '/country?token='.getenv('IPINFO_TOKEN'));
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
@@ -447,7 +448,7 @@ class UserController extends Controller {
             CURLOPT_URL => 'https://ipinfo.io/' . $ip . '/country?token='.getenv('IPINFO_TOKEN'),
             CURLOPT_SSL_VERIFYPEER => 0
         ));
-        $resp = json_decode(curl_exec($curl));
+        $resp = curl_exec($curl);
         curl_close($curl);
 
         var_dump($resp);
