336b3175abd8d68c03e49471b43278a741810054
unknown <miroslav.nedelchev@dentacoin.com>
2020-04-09 08:30:21 +0300

    test combined login

--- app/Http/Controllers/DentacoinLoginGateway.php
+++ app/Http/Controllers/DentacoinLoginGateway.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 class DentacoinLoginGateway extends Controller
 {
     public function getView(Request $request)   {
-        $user_ip = $request->input('user_ip');
-        if (empty($user_ip)) {
-            return response()->json(['error' => false, 'message' => 'Missing user IP.']);
-        }
-
         $view = view('partials/dentacoin-login-gateway', ['client_ip' => $request->input('user_ip'), 'type' => $request->input('type')]);
         $view = $view->render();
 
