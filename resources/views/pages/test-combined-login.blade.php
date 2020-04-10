b08ea10aa0f7f590042a34cf82dd2de93f7fb35c
unknown <miroslav.nedelchev@dentacoin.com>
2020-04-10 17:07:26 +0300

    test combined-login

--- resources/views/pages/test-combined-login.blade.php
+++ resources/views/pages/test-combined-login.blade.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <title>Page Title</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
     {{--<script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
