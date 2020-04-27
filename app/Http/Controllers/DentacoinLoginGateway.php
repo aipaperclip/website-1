a9e2d6294517b9e06536501e5f197a027b9159ec
unknown <miroslav.nedelchev@dentacoin.com>
2020-04-27 11:36:21 +0300

    check langaugesg

--- app/Http/Controllers/DentacoinLoginGateway.php
+++ app/Http/Controllers/DentacoinLoginGateway.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 use Illuminate\Support\Facades\Lang;
 
 class DentacoinLoginGateway extends Controller
@@ -11,6 +12,7 @@ class DentacoinLoginGateway extends Controller
         $params = array(
             'type' => $request->input('type')
         );
+        App::setlocale('en');
 
         $translations = Lang::get('login-register');
         var_dump($translations);die('asd');
