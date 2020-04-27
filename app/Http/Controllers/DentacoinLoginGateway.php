8a4e7fd89899cd94ff3200248951967c5da1e0af
unknown <miroslav.nedelchev@dentacoin.com>
2020-04-27 11:31:12 +0300

    check langaugesg

--- app/Http/Controllers/DentacoinLoginGateway.php
+++ app/Http/Controllers/DentacoinLoginGateway.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 class DentacoinLoginGateway extends Controller
 {
@@ -10,6 +11,7 @@ class DentacoinLoginGateway extends Controller
         $params = array(
             'type' => $request->input('type')
         );
+        App::setlocale('en');
 
         if ($params['type'] == 'incompleted-dentist-register') {
             $staging = $request->input('staging');
