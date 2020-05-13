01ee16db355e7f6c45b1c34d10ed878abfb86202
unknown <miroslav.nedelchev@dentacoin.com>
2020-05-13 13:24:56 +0300

    switch get enums from dev to live

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -101,6 +101,9 @@ class APIRequestsController extends Controller {
                 break;
         }
 
+        var_dump($post_fields_arr);
+        die('asd');
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
