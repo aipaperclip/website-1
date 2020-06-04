6162934ac53e8528c7364abdf423add2580fca47
unknown <miroslav.nedelchev@dentacoin.com>
2020-06-04 10:55:15 +0300

    add hub redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,7 +42,10 @@ class HomeController extends Controller
     public function getView() {
         if((new UserController())->checkSession()) {
             //return $this->getLoggedView();
-            return Redirect::to('http://hub.dentacoin.com');
+            $slug = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['id'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY'));
+            $type = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['type'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY'));
+            $token = (new \App\Http\Controllers\Controller())->encrypt(session('logged_user')['token'], getenv('API_ENCRYPTION_METHOD'), getenv('API_ENCRYPTION_KEY'));
+            return Redirect::to('http://hub.dentacoin.com/custom-cookie?slug='.urlencode($slug).'&type='.urlencode($type).'&token='.urlencode($token));
         } else {
             return $this->getNotLoggedView();
         }
