d472fa6b7796c963d69855a7090adca6acf1cd9c
unknown <miroslav.nedelchev@dentacoin.com>
2020-07-17 16:56:12 +0300

    fix cross login error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -147,10 +147,12 @@ class UserController extends Controller {
             $type = $this->decrypt(Input::get('type'));
             $token = $this->decrypt(Input::get('token'));
 
-            $user = (new APIRequestsController())->getUserData($slug, true);
-            if ($user) {
+            $userData = (new APIRequestsController())->getUserData($slug, true);
+            if (!empty($userData) && is_object($userData) && property_exists($userData, 'success') && $userData->success) {
+                $user = $userData->data;
                 $approved_statuses = array('approved','test','added_by_clinic_claimed','added_by_dentist_claimed');
-                if ($user->self_deleted != NULL) {
+
+                if (property_exists($user, 'self_deleted') && $user->self_deleted != NULL) {
                     return abort(404);
                 } else if (!in_array($user->status, $approved_statuses)) {
                     return abort(404);
