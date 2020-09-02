921cb5da624708d7e09a2cb6f57f0af3f8df0337
unknown <miroslav.nedelchev@dentacoin.com>
2020-09-02 14:22:05 +0300

    fix header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -155,7 +155,7 @@
             </div>
         </div>
     </header>
-    <main class="main-container">@yield("content")</main>
+    <main @if(!empty(Route::current()) && (Route::current()->getName() != 'home' && Route::current()->getName() != 'users' && Route::current()->getName() != 'dentists' && Route::current()->getName() != 'traders')) class="main-container" @endif>@yield("content")</main>
     <footer class="padding-bottom-80 @if(!empty(Route::current()) && Route::current()->getName() == 'traders') black-style @endif">
         <div class="container">
             @if(!empty($socials))
