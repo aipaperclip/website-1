da9ac5c41e479cc4429a82a8906b85f2f1b2ae6f
unknown <miroslav.nedelchev@dentacoin.com>
2020-10-22 11:26:55 +0300

    include clientVersion when logging in

--- public/assets/libs/civic-login/civic-combined-login.js
+++ public/assets/libs/civic-login/civic-combined-login.js
@@ -1,4 +1,14 @@
 (async function() {
+    function isMobile() {
+        var isMobile = false; //initiate as false
+// device detection
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)))  {
+            isMobile = true;
+        }
+        return isMobile;
+    }
+
     console.log('civic loaded');
     await $.getScript('https://dentacoin.com/assets/libs/civic-login/civic-config.js', function() {});
 
@@ -50,25 +60,25 @@
     // Listen for data
     civicSip.on('auth-code-received', function (event) {
         var jwtToken = event.response;
-        if (civicActionType == 'register') {
-            if (civicApiVersion == 'v2') {
-                // this should work on first phase
-                // old legacy app
-                console.log('stop civic register');
+        console.log(civicActionType, 'civicActionType uth-code-received');
+        console.log(civicApiVersion, 'civicApiVersion uth-code-received');
 
-                var data = {};
-                data.tempData = {
-                    civicActionType: civicActionType,
-                    civicAjaxUrl: civicAjaxUrl
-                };
-                console.log(data, 'auth-code-received');
+        if (civicActionType == undefined) {
+            console.log('redirect from civic app)');
+        } else {
+            if (civicActionType == 'register') {
+                if (civicApiVersion == 'v2') {
+                    // this should work on first phase
+                    // old legacy app
+                    console.log('stop civic register');
 
-                customCivicEvent('CivicLegacyAppForbiddenRegistrations', 'Registering via Civic Legacy App is forbidden.', data);
-            } else {
+                    customCivicEvent('CivicLegacyAppForbiddenRegistrations', 'Registering via Civic Legacy App is forbidden.');
+                } else if (civicApiVersion == 'v3') {
+                    proceedWithDentacoinAuth(jwtToken);
+                }
+            } else if (civicActionType == 'login') {
                 proceedWithDentacoinAuth(jwtToken);
             }
-        } else {
-            proceedWithDentacoinAuth(jwtToken);
         }
     });
 
@@ -85,7 +95,8 @@
                     customCivicEvent('noUserIdReceived', 'No userId found after civic token/data exchange.', ret);
                 } else {
                     customCivicEvent('userIdReceived', 'UserId found after civic token/data exchange.', ret);
-                    var register_data = {
+                    var loginRegisterData = {
+                        clientVersion: civicApiVersion,
                         auth_token: jwtToken,
                         social_network: 'civic',
                         type: 'patient'
@@ -95,7 +106,7 @@
 
                     if (civic_custom_btn.length) {
                         currentPlatform = civic_custom_btn.attr('data-platform');
-                        register_data.platform = currentPlatform;
+                        loginRegisterData.platform = currentPlatform;
                     } else {
                         // civic popup called by library get param condition and not, because of Dentacoin login call
                         if (location.hostname == 'dentacoin.com') {
@@ -109,13 +120,13 @@
                         } else if (location.hostname == 'assurance.dentacoin.com') {
                             currentPlatform = 'assurance';
                         }
-                        register_data.platform = currentPlatform;
+                        loginRegisterData.platform = currentPlatform;
                     }
 
                     console.log(currentPlatform, 'currentPlatform');
 
                     if (dcnGateway.utils.cookies.get('first_test') != '') {
-                        register_data.country_id = JSON.parse(decodeURIComponent(dcnGateway.utils.cookies.get('first_test')))['location'];
+                        loginRegisterData.country_id = JSON.parse(decodeURIComponent(dcnGateway.utils.cookies.get('first_test')))['location'];
                     }
 
                     if ($('.patient .form-register [name="user_patient_type[]"]:checked').val() != 'undefined') {
@@ -124,17 +135,17 @@
                             tempArr.push($('.patient .form-register [name="user_patient_type[]"]:checked').eq(i).val());
                         }
 
-                        register_data.user_patient_type = JSON.stringify(tempArr);
+                        loginRegisterData.user_patient_type = JSON.stringify(tempArr);
                     }
 
                     setTimeout(function () {
                         if (civic_custom_btn.length) {
                             if (civic_custom_btn.attr('data-inviter') != undefined) {
-                                register_data.invited_by = civic_custom_btn.attr('data-inviter');
+                                loginRegisterData.invited_by = civic_custom_btn.attr('data-inviter');
                             }
 
                             if (civic_custom_btn.attr('data-inviteid') != undefined) {
-                                register_data.inviteid = civic_custom_btn.attr('data-inviteid');
+                                loginRegisterData.inviteid = civic_custom_btn.attr('data-inviteid');
                             }
                         }
 
@@ -142,7 +153,7 @@
                             type: 'POST',
                             url: civicAjaxUrl,
                             dataType: 'json',
-                            data: register_data,
+                            data: loginRegisterData,
                             success: function(data) {
                                 if (data.success) {
                                     if (data.deleted) {
@@ -195,14 +206,13 @@
                                             if (civicApiVersion == 'v2') {
                                                 // this should work on second phase
                                                 console.log('stop civic login');
-
-                                                data.tempData = {
-                                                    civicActionType: civicActionType,
-                                                    civicAjaxUrl: civicAjaxUrl
-                                                };
-
-                                                console.log('CivicLegacyAppForbiddenLogging');
-                                                customCivicEvent('CivicLegacyAppForbiddenLogging', 'Logging via Civic Legacy App is forbidden.', data);
+                                                if (isMobile()) {
+                                                    console.log('logging from phone');
+                                                    customCivicEvent('CivicLegacyAppForbiddenRegistrations', 'Registering via Civic Legacy App is forbidden.');
+                                                } else {
+                                                    console.log('CivicLegacyAppForbiddenLogging');
+                                                    customCivicEvent('CivicLegacyAppForbiddenLogging', 'Logging via Civic Legacy App is forbidden.', data);
+                                                }
                                             } else {
                                                 customCivicEvent('patientProceedWithCreatingSession', 'Request to CoreDB-API succeed.', data);
                                             }
