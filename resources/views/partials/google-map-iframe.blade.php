bb69a6034784d96254fe7481a46d9f1fed7d3e2e
unknown <miroslav.nedelchev@dentacoin.com>
2020-10-30 08:22:51 +0200

    push map changes

--- resources/views/partials/google-map-iframe.blade.php
+++ resources/views/partials/google-map-iframe.blade.php
@@ -9,14 +9,14 @@
         var HOME_URL = '{{ route("home") }}';
     </script>
     <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css">
-    <link rel="stylesheet" type="text/css" href="/dist/css/front-style.css">
+    <link rel="stylesheet" type="text/css" href="/dist/css/front-style.css?v={{time()}}">
 </head>
 <body class="dentacoin-map-iframe">
     <div class="camping-loader"></div>
     @php($type = \Illuminate\Support\Facades\Input::get('type'))
     <section class="section-google-map module @if ($type == 'dentists') dentists-color-style @endif"><div class="map-container"></div></section>
     <script src="/dist/js/front-libs-script.js"></script>
-    <script src="/dist/js/front-script.js"></script>
+    <script src="/dist/js/front-script.js?v={{time()}}"></script>
     <script>
         projectData.general_logic.data.showLoader();
         projectData.general_logic.data.dentacoinGoogleMap();
