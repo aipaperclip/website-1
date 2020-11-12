abb103ac69ca612574e3a96495c7a023334f04ec
unknown <miroslav.nedelchev@dentacoin.com>
2020-11-12 16:35:39 +0200

    test combined login mobile login

--- app/Http/Controllers/DentacoinLoginGateway.php
+++ app/Http/Controllers/DentacoinLoginGateway.php
@@ -82,8 +82,11 @@ class DentacoinLoginGateway extends Controller
             'email.required' => 'Email is required.',
             'email.email' => 'Invalid email.'
         ]);
+        var_dump($request->input());
 
         $response = (new APIRequestsController())->checkIfCivicEmailTryingToLoginFromMobileApp($request->input('email'));
+        var_dump($response);
+        die();
 
         if (!empty($response) && is_object($response) && property_exists($response, 'success') && $response->success) {
             return response()->json(['success' => true]);
