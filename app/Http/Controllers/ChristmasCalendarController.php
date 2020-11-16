cbdc45551cc11df9d6ad76b4d937ab235bd23dd3
unknown <miroslav.nedelchev@dentacoin.com>
2020-11-16 11:46:17 +0200

    test calendar

--- app/Http/Controllers/ChristmasCalendarController.php
+++ app/Http/Controllers/ChristmasCalendarController.php
@@ -81,11 +81,7 @@ class ChristmasCalendarController extends Controller
     public function getTaskPopup($year, $id) {
         // if ((new UserController())->checkSession() && in_array(session('logged_user')['id'], self::ALLOWED_ACCOUNTS)) {
         if ((new UserController())->checkSession() /*&& strtotime('12/01/2019') < time()*/) {
-            var_dump($id);
             $task = ChristmasCalendarTask::where(array('id' => $id, 'year' => $year))->get()->first();
-
-            var_dump($task);
-            die();
 
             //$participant = ChristmasCalendarParticipant::where(array('user_id' => session('logged_user')['id']))->get()->first();
             $participant = ChristmasCalendarParticipant::where(array('user_id' => session('logged_user')['id']))->get()->first();
